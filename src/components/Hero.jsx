29ffc66c7b25e9e28958f643b91555201697a0ca
manoj-adhikari <imadhikarimanoj@gmail.com>
2024-09-17 02:39:39 +0530

    fixing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import Typewriter from "typewriter-effect";
 
+
 const Hero = () => {
   return (
     <section 
@@ -10,7 +11,7 @@ const Hero = () => {
         {/* Profile Photo */}
         <div className="w-32 h-32 rounded-full border-4 border-black bg-gray-200 shadow-[8px_8px_0_rgba(0,0,0,1)] overflow-hidden mr-6">
           <img
-            src="/public/images/profile.png" // Replace with your profile photo URL
+            src="/images/profile.png" // Replace with your profile photo URL
             alt="Profile"
             className="w-full h-full object-cover"
           />
